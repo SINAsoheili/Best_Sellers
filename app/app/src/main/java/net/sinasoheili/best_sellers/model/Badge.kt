08941a485097d3d8f09144f111e09094a84ae898
SINA <sinasoheili79@gmail.com>
2021-05-13 20:24:19 +0430

    add category field to Badge

--- app/app/src/main/java/net/sinasoheili/best_sellers/model/Badge.kt
+++ app/app/src/main/java/net/sinasoheili/best_sellers/model/Badge.kt
@@ -1,3 +1,3 @@
 package net.sinasoheili.best_sellers.model
 
-data class Badge constructor(val id: Int, val name: String, val minScore: Int)
+data class Badge constructor(val id: Int, val name: String, val minScore: Int , val category: String)
