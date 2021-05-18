94b05fd63175e40c6d78321a6020ccc78f125522
SINA <sinasoheili79@gmail.com>
2021-05-18 15:39:30 +0430

    add bottom navigation view to seller main page and creaate menu to show item of bottom navigation view

--- app/app/src/main/java/net/sinasoheili/best_sellers/view/SellerMainPage.kt
+++ app/app/src/main/java/net/sinasoheili/best_sellers/view/SellerMainPage.kt
@@ -2,11 +2,42 @@ package net.sinasoheili.best_sellers.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.FrameLayout
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import net.sinasoheili.best_sellers.R
 
-class SellerMainPage : AppCompatActivity() {
+class SellerMainPage : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var bnv: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seller_main_page)
+
+        initObj()
+    }
+
+    private fun initObj() {
+        bnv = findViewById(R.id.bnv_sellerMainPage)
+        bnv.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.item_bnv_statistic -> {
+                Toast.makeText(this , item.title , Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.item_bnv_dashboard -> {
+                Toast.makeText(this , item.title , Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.item_bnv_store -> {
+                Toast.makeText(this , item.title , Toast.LENGTH_SHORT).show()
+            }
+        }
+        return true
     }
 }
