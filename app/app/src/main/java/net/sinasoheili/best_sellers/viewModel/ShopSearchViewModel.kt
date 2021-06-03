1a7544f26b0977342ecb6f71bf3fdddf7d7bd691
SINA <sinasoheili79@gmail.com>
2021-06-03 09:11:18 +0430

    create viewModel for shopSearch Activity

---
+++ app/app/src/main/java/net/sinasoheili/best_sellers/viewModel/ShopSearchViewModel.kt
@@ -0,0 +1,46 @@
+package net.sinasoheili.best_sellers.viewModel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import net.sinasoheili.best_sellers.model.Criteria
+import net.sinasoheili.best_sellers.model.Shop
+import net.sinasoheili.best_sellers.model.ShopCategory
+import net.sinasoheili.best_sellers.repository.CategoryRepository
+import net.sinasoheili.best_sellers.repository.ShopRepository
+import net.sinasoheili.best_sellers.util.DataState
+
+class ShopSearchViewModel constructor(val categoryRepository: CategoryRepository ,
+                                      val shopRepository: ShopRepository): ViewModel() {
+
+    val categoriesData: MutableLiveData<DataState<List<ShopCategory>>> = MutableLiveData()
+    val criteriaData: MutableLiveData<DataState<List<Criteria>>> = MutableLiveData()
+    val shopSearchData: MutableLiveData<DataState<List<Shop>>> = MutableLiveData()
+
+    fun getCategories() {
+        viewModelScope.launch {
+            categoryRepository.getShopCategories().onEach {
+                categoriesData.value = it
+            }.launchIn(viewModelScope)
+        }
+    }
+
+    fun getCriteria(categoryId: Int) {
+        viewModelScope.launch {
+            categoryRepository.getCriteria(categoryId).onEach {
+                criteriaData.value = it
+            }.launchIn(viewModelScope)
+        }
+    }
+
+    fun searchShop(categoryId: Int , criteriaId: Int) {
+        viewModelScope.launch {
+            shopRepository.searchShop(categoryId , criteriaId ).onEach {
+                shopSearchData.value = it
+            }.launchIn(viewModelScope)
+        }
+    }
+}
