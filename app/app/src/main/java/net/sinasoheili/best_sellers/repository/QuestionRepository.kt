f486207012b2d6603df608687d1b9542f9d8a702
SINA <sinasoheili79@gmail.com>
2021-06-11 11:22:15 +0430

    implement functions to check user answer to question or not . and also remove survey

--- app/app/src/main/java/net/sinasoheili/best_sellers/repository/QuestionRepository.kt
+++ app/app/src/main/java/net/sinasoheili/best_sellers/repository/QuestionRepository.kt
@@ -5,16 +5,17 @@ import android.util.Log
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
 import net.sinasoheili.best_sellers.R
+import net.sinasoheili.best_sellers.model.AnsweredQuestion
 import net.sinasoheili.best_sellers.model.Question
 import net.sinasoheili.best_sellers.util.CacheToPreference
 import net.sinasoheili.best_sellers.util.DataState
 import net.sinasoheili.best_sellers.util.Keys
 import net.sinasoheili.best_sellers.webService.*
-import java.lang.Exception
 
 class QuestionRepository constructor(val context: Context,
                                      private val webService: WebService ,
-                                     private val questionMapper: QuestionMapper)
+                                     private val questionMapper: QuestionMapper,
+                                     private val answeredQuestionMapper: AnsweredQuestionMapper)
 {
     suspend fun getCategoryQuestion(categoryId: Int) = flow<DataState<MutableList<Question>>> {
         emit(DataState.Loading())
@@ -47,6 +48,53 @@ class QuestionRepository constructor(val context: Context,
                 emit(DataState.Success(true))
             } else {
                 emit(DataState.Error(context.getString(R.string.question_not_register)))
+            }
+
+        } catch (e: Exception) {
+            emit(DataState.ConnectionError(e))
+        }
+    }
+
+    suspend fun checkUserAnsQuestion(shopId: Int) = flow<DataState<Boolean>> {
+        emit(DataState.Loading())
+        delay(1000)
+
+        try {
+            val uaqe: UserAnsQuestionEntity = webService.checkUserAnsQuestion(getUserIdFromCache() , shopId)
+            emit(DataState.Success(uaqe.userAnsStatus))
+
+        } catch(e: Exception) {
+            emit(DataState.ConnectionError(e))
+        }
+    }
+
+    suspend fun getUserAnsweredQuestion(shopId: Int) = flow<DataState<List<AnsweredQuestion>>> {
+        emit(DataState.Loading())
+        delay(1000)
+
+        try {
+            val aqer: AnsweredQuestionEntityResponse= webService.getUserAnsweredQuestion(getUserIdFromCache(), shopId)
+            val result : List<AnsweredQuestionEntity> = aqer.result
+            val response: MutableList<AnsweredQuestion> = mutableListOf()
+            for(i in result) {
+                response.add(answeredQuestionMapper.toBase(i))
+            }
+            emit(DataState.Success(response))
+        } catch (e: Exception) {
+            emit(DataState.ConnectionError(e))
+        }
+    }
+
+    suspend fun removeSurvey(shopId: Int) = flow<DataState<Boolean>> {
+        emit(DataState.Loading())
+        delay(1000)
+
+        try {
+            val dsr: DeleteSurveyResponse = webService.removeSurvey(getUserIdFromCache(), shopId)
+            if(dsr.result) {
+                emit(DataState.Success(true))
+            } else {
+                emit(DataState.Error(context.getString(R.string.unknown_error_happen)))
             }
 
         } catch (e: Exception) {
