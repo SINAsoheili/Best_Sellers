e5435ff679b46a3f9aa09bbe2743517f70c3ed90
SINA <sinasoheili79@gmail.com>
2021-06-24 13:13:15 +0430

    show chart in statistic page and also set new adapter for user messages

--- app/app/src/main/java/net/sinasoheili/best_sellers/di/RetrofitModule.kt
+++ app/app/src/main/java/net/sinasoheili/best_sellers/di/RetrofitModule.kt
@@ -12,7 +12,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 @InstallIn(ActivityComponent::class)
 object RetrofitModule {
 
-    private const val BASE_URL: String = "http://10.0.2.2:5000/"
+//    private const val BASE_URL: String = "http://10.0.2.2:5000/"
+    private const val BASE_URL: String = "http://bcfd6bcd1934.ngrok.io"
 
     @Provides
     fun getRetrofit() : Retrofit {
--- app/app/src/main/java/net/sinasoheili/best_sellers/view/SellerStatisticFragment.kt
+++ app/app/src/main/java/net/sinasoheili/best_sellers/view/SellerStatisticFragment.kt
@@ -1,36 +1,55 @@
 package net.sinasoheili.best_sellers.view
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import net.sinasoheili.best_sellers.R
 import net.sinasoheili.best_sellers.model.Message
 import net.sinasoheili.best_sellers.model.Statistic
 import net.sinasoheili.best_sellers.util.DataState
+import net.sinasoheili.best_sellers.util.UserMessageAdapter
 import net.sinasoheili.best_sellers.viewModel.SellerStatisticViewModel
 import javax.inject.Inject
 
+
 @AndroidEntryPoint
-class SellerStatisticFragment: Fragment(R.layout.fragment_statistics_seller), View.OnClickListener {
+class SellerStatisticFragment: Fragment(R.layout.fragment_statistics_seller) {
 
     @Inject
     lateinit var viewModel: SellerStatisticViewModel
 
-    private lateinit var tvStatistic: TextView
     private lateinit var progressBar: ProgressBar
-    private lateinit var btnShowMessage: Button
     private lateinit var lvMessage: ListView
+    private lateinit var barChart: BarChart
+    private lateinit var tvEmptyList: TextView
+    private lateinit var tvUserMessageTitle: TextView
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        requireActivity().window.statusBarColor = requireContext().getColor(R.color.light_green)
 
         setObserver()
         viewModel.getStatistic()
+        viewModel.getMessages()
 
         return super.onCreateView(inflater, container, savedInstanceState)
     }
@@ -42,18 +61,20 @@ class SellerStatisticFragment: Fragment(R.layout.fragment_statistics_seller), Vi
     }
 
     private fun initObj(view: View) {
-        tvStatistic = view.findViewById(R.id.tv_fragmentStatisticSeller_statistic)
+        tvEmptyList = view.findViewById(R.id.tv_fragmentStatisticSeller_emptyList)
 
         progressBar = view.findViewById(R.id.pb_fragmentStatisticSeller)
 
-        btnShowMessage = view.findViewById(R.id.btn_fragmentStatisticSeller_showMessage)
-        btnShowMessage.setOnClickListener(this)
-
         lvMessage = view.findViewById(R.id.lv_fragmentStatisticSeller_messages)
+
+        barChart = view.findViewById(R.id.chart_fragmentStatisticSeller_statistic)
+        initChart()
+
+        tvUserMessageTitle = view.findViewById(R.id.tv_fragmentStatisticSeller_title)
     }
 
     private fun setObserver() {
-        viewModel.statisticData.observe(viewLifecycleOwner , Observer {
+        viewModel.statisticData.observe(viewLifecycleOwner, Observer {
             when (it) {
                 is DataState.Success -> {
                     invisibleProgressBar()
@@ -71,13 +92,13 @@ class SellerStatisticFragment: Fragment(R.layout.fragment_statistics_seller), Vi
             }
         })
 
-        viewModel.messageData.observe(viewLifecycleOwner , Observer {
-            when(it) {
+        viewModel.messageData.observe(viewLifecycleOwner, Observer {
+            when (it) {
                 is DataState.Success -> {
                     invisibleProgressBar()
 
-                    if(it.data.isEmpty()) {
-                        showMessage(requireContext().getString(R.string.message_not_found))
+                    if (it.data.isEmpty()) {
+                        emptyListAlert()
                     } else {
                         showMessageList(it.data)
                     }
@@ -109,29 +130,63 @@ class SellerStatisticFragment: Fragment(R.layout.fragment_statistics_seller), Vi
     }
 
     private fun showMessage(text: String) {
-        Snackbar.make(progressBar , text, Snackbar.LENGTH_LONG)
+        Snackbar.make(progressBar, text, Snackbar.LENGTH_LONG)
                 .setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE)
                 .show()
     }
 
-    private fun showStatistic(statistics: ArrayList<Statistic>) {
+    private fun initChart() {
+        barChart.description = null
 
-        tvStatistic.text = ""
-        for (i in statistics) {
-            tvStatistic.append(i.toString()+"\n")
+        val xAxis: XAxis = barChart.xAxis
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        xAxis.labelRotationAngle = 15F
+        xAxis.setDrawGridLines(false)
+
+        val yAxisRight: YAxis = barChart.axisRight
+        val yAxisLeft: YAxis = barChart.axisLeft
+        yAxisRight.isEnabled = false
+        yAxisLeft.isEnabled = false
+    }
+
+    private fun showStatistic(statistics: ArrayList<Statistic>) {
+        val entries: MutableList<BarEntry> = mutableListOf()
+
+        for(i in 0 until statistics.size) {
+            entries.add(BarEntry(i.toFloat(), statistics.get(i).amount))
+        }
+
+        val dataSet: BarDataSet = BarDataSet(entries, null)
+        dataSet.color = requireContext().getColor(R.color.green)
+
+        val barData: BarData = BarData(dataSet)
+        barChart.data = barData
+        barChart.invalidate()
+
+        val titles: List<String> = List(statistics.size) { i: Int -> statistics.get(i).title }
+
+        val xAxis: XAxis = barChart.xAxis
+        xAxis.labelCount = titles.size
+        xAxis.valueFormatter = object:IndexAxisValueFormatter() {
+            override fun getFormattedValue(value: Float): String {
+                return titles.get(value.toInt())
+            }
         }
     }
 
     private fun showMessageList(messages: List<Message>) {
-        val adapter: ArrayAdapter<Message> = ArrayAdapter(requireContext()  , android.R.layout.simple_list_item_1 , messages)
+        lvMessage.visibility = View.VISIBLE
+        tvUserMessageTitle.visibility = View.VISIBLE
+        tvEmptyList.visibility =  View.GONE
+
+        val adapter: UserMessageAdapter = UserMessageAdapter(requireContext() , messages)
         lvMessage.adapter = adapter
     }
 
-    override fun onClick(v: View?) {
-        when(v) {
-            btnShowMessage -> {
-                viewModel.getMessages()
-            }
-        }
+    private fun emptyListAlert() {
+        lvMessage.visibility = View.GONE
+        tvUserMessageTitle.visibility = View.GONE
+        tvEmptyList.visibility = View.VISIBLE
+        tvEmptyList.text = requireContext().getString(R.string.message_not_found)
     }
 }
